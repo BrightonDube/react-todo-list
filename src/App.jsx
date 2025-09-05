239bd0c8c881d9fa854028a8b46835ab1608e678
BrightonDube <BrightonDube@users.noreply.github.com>
2025-09-05 23:55:40 +0200

    feat: refactor App component to manage todos with localStorage and integrate NewTodoForm and TodoList components

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,52 @@
-import { useState } from 'react'
-import './styles.css'
-export default function App(){
-  
-  const [todos, setTodos] = useState([])
-  
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import "./styles.css"
+import { TodoList } from "./TodoList"
+
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+
   function toggleTodo(id, completed) {
     setTodos(currentTodos => {
       return currentTodos.map(todo => {
-        if (todo.id === id){
-          return {...todo, completed}
+        if (todo.id === id) {
+          return { ...todo, completed }
         }
+
         return todo
       })
     })
   }
+
   function deleteTodo(id) {
     setTodos(currentTodos => {
       return currentTodos.filter(todo => todo.id !== id)
     })
   }
-  return <>
-  <form onSubmit={handleSubmit} className='new-item-form'>
-    <div className="form-row">
-      <label htmlFor="item">New Item</label>
-      <input 
-      type="text" 
-      value={newItem} 
-      id='item' 
-      onChange={ e => setNewItem(e.target.value)
-      } />
-    </div>
-      <button className='btn'>Add</button>    
-  </form>
-  <h1 className='header'>Todo List</h1>
-  <ul className="list">
-    {todos.length === 0 && "No Todos"}
-    {todos.map(todo => {
-      return <li key={todo.id}>
-      <label>
-        <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
-        {todo.title}
-      </label>
-      <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
-      {/* pass an arrow function don't call the function in the button directly */}
-    </li>
-    })}
-    
-  </ul>
-  </>
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  )
 }
--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -13,4 +13,16 @@ export function NewTodoForm({onSubmit}) {
     })
     setNewItem("")
   }
+  return <form onSubmit={handleSubmit} className='new-item-form'>
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input 
+      type="text" 
+      value={newItem} 
+      id='item' 
+      onChange={ e => setNewItem(e.target.value)
+      } />
+    </div>
+      <button className='btn'>Add</button>    
+  </form>
 }
